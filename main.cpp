0da88e085c1ff7a1f500827343f396e010eacc22
vNemetz <vininemetz@gmail.com>
2026-05-11 20:29:00 -0300

    feat: including SFML as a static library

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
-int main(int argc, char *argv[]){
-  return 0;
+#include <SFML/Graphics.hpp>
+
+int main() {
+    // Cria uma janela (Requisito 3 do projeto)
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Teste SFML - Simulador");
+    
+    // Um simples circulo para testar a renderizacao
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Cyan);
+    shape.setPosition(100.f, 100.f);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
